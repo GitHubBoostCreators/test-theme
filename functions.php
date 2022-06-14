d877958ceeb0c01ecc7a7871dd7137e76094f4d0
RogerRozjeeDesign <r.paulussen@rozjee.nl>
2022-06-14 15:28:16 +0200

    d

--- functions.php
+++ functions.php
@@ -1,3 +1,16 @@
 <?php
 // Exit if accessed directly.
 defined( 'ABSPATH' ) || exit;
+
+if( ! class_exists( 'BC_Updater' ) ){
+	include_once( plugin_dir_path( __FILE__ ) . 'updater.php' );
+}
+
+// Explain updater https://www.smashingmagazine.com/2015/08/deploy-wordpress-plugins-with-github-using-transients/
+$updater = new BC_Updater( __FILE__ );
+$updater->set_username( 'GitHubBoostCreators' );
+$updater->set_repository( 'test-theme' );
+/*
+	$updater->authorize( 'abcdefghijk1234567890' ); // Your auth code goes here for private repos
+	*/
+$updater->initialize();
